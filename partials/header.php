2ae63d0e4f08d05b4f9cb983846d0b3ee9fe1897
Your Name <you@example.com>
2024-05-20 15:58:39 +0100

    gsdfsfd

--- partials/header.php
+++ partials/header.php
@@ -1,5 +1,6 @@
 <?php
-define('BASE_PATH', 'http://localhost:8040/fastburgers')
+//define('BASE_PATH', 'http://localhost:8040/fastburgers')
+define('BASE_PATH', 'http://localhost:8010/fastburgers')
 <!DOCTYPE html>
 <html lang="en">
 <head>
